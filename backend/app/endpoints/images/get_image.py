d9acc725fa9e5ddad170aaeef2772d4fdcf96419
Alexey Guskov <alexey.guskov@ombori.com>
2023-08-21 10:16:11 +0200

    feat: can list files and see the content

--- backend/app/endpoints/images/get_image.py
+++ backend/app/endpoints/images/get_image.py
@@ -1,13 +1,24 @@
-from uuid import UUID
+import os
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
+from fastapi.responses import FileResponse
 
 router = APIRouter()
 
+from app.config import config
 
 @router.get(
     "/images/{file_stem}",
     status_code=status.HTTP_200_OK,
 )
 async def get_image(file_stem: str):
-    pass
+    print(file_stem, flush=True)
+    if not file_stem.isalnum():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File stem must be alphanumeric")
+    
+    file_path = f"{config.images_dir}/{file_stem}.jpg"  
+
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        return FileResponse(file_path)
+    else:
+        return status.HTTP_404_NOT_FOUND
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,39 @@
-export const App = () => {
-  const helloWorld = () => {
-    // fetch hello world endpoint
-    return "hello world";
-  };
+import { useEffect, useState } from 'react';
 
-  return <div>{helloWorld()}</div>;
+const domain = 'http://localhost:8000';
+
+// TODO: local file cache
+
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [selection, setSelection] = useState(null);
+
+  // todo force reload button
+  useEffect(() => {
+    const load = async () => {
+      console.log('loading');
+      try {
+        const res = await fetch(domain + '/images');
+        setImages(await res.json());
+      } catch (e) {
+        console.error('fail to load', e.toString());
+        // TODO: show error message
+      }
+    }
+    load();
+  }, []);
+
+  return <div>
+    {images.map(f => (
+      <div>
+        <button style={{ background: f.file_stem === selection ? 'lightgray' : 'none', border: 'none' }} onClick={() => setSelection(f.file_stem)} >
+          {f.file_stem}
+        </button>
+      </div>
+    ))}
+    {images.length === 0 && <div>nothing loaded</div>}
+    {selection && images.find(a => a.file_stem === selection) && (
+      <img src={`${domain}/images/${selection}`}></img>
+    )}
+  </div >;
 };
